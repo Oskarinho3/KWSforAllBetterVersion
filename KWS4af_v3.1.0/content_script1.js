b287d0119f34fb2d18d15e7a01521cf1cdcda87f
KWS4ALL <kwsforall@proton.me>
2024-02-21 14:48:49 +0100

    Fix links

--- KWS4af_v3.1.0/content_script1.js
+++ KWS4af_v3.1.0/content_script1.js
@@ -4,11 +4,11 @@
      scr.innerHTML = `const LoadScript = (url, cb) => { fetch(url).then(res => res.json()).then((out) => { cb(out) }).catch(err => { throw err }); }
  
  const links = {
-    "link1":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBalls/ballExp.js",
-    "link2":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBalls/ballUpgrade.js",
-    "link3":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBalls/ballReset.js",
-    "link4":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBalls/ballManager.js",
-    "link5":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBalls/script1-2.js",
+    "link1":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBallsFixText/ballExp.js",
+    "link2":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBallsFixText/ballUpgrade.js",
+    "link3":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBallsFixText/ballReset.js",
+    "link4":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBallsFixText/ballManager.js",
+    "link5":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/addBallsFixText/script1-2.js",
  };
  
  const fetchPromises = Object.values(links).map(link => {
