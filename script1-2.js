bba4ae6f4fa2906c24fc2eb41f3293f79059ce0f
KWS4ALL <kwsforall@proton.me>
2024-02-28 09:55:16 +0100

    Add delay for tournament sign

--- script1-2.js
+++ script1-2.js
@@ -1443,8 +1443,10 @@ if (typeof GAME === 'undefined') { } else {
             }
             handleTournamentsSign() {
                 if(this.tourSigned) { return }
-                var currentServerHour = new Date(GAME.getTime()*1000).getHours();
-                if(currentServerHour >= 18 && currentServerHour < 21) {
+                var currentServerTime = new Date(GAME.getTime()*1000);
+                var currentServerHour = currentServerTime.getHours();
+                var currentServerMinute = currentServerTime.getMinutes();
+                if((currentServerHour == 18 && currentServerMinute > 10) || (currentServerHour > 18 && currentServerHour < 21)) {
                     var tourSignButton = $("[data-option=tournament_sign]");
                     if(tourSignButton.length == 0) {
                         GAME.emitOrder({a:57,type:0,type2:0,page:2});
