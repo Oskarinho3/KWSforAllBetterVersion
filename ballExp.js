3970a9c0c350c98575102af4e980be4b178caa5e
KWS4ALL <kwsforall@proton.me>
2024-02-28 08:33:37 +0100

    Add button to exp ball without stoping on next level reached

--- ballExp.js
+++ ballExp.js
@@ -1,19 +1,27 @@
 class ballExp {
     constructor() {
         this.expText = 'Exp do NEXT';
+        this.expNonStopText = 'Exp bez przerwy';
         this.stopText = 'STOP';
         this.synergy = parseInt($("#ss_synergy_lvl").html());
         this.hasStarted = false;
+        this.nonStopExp = false;
         $("body").on("click", `button[data-option="ss_page"][data-page="upgrade"]`, () => {
-            this.showExpButton();
+            this.showExpButtons();
         });
         $("body").on("click", `button[data-option="ss_page"][data-page="reset"], #soulstone_interface .closeicon`, () => {
             if(this.hasStarted) {
                 this.stopUpgrading();
             }
-            this.hideExpButton();
+            this.hideExpButtons();
         });
         $("body").on("click", `button[data-option="ss_lvlup_next"]`, () => {
+            this.controller();
+        });
+        $("body").on("click", `button[data-option="ss_lvlup_nonstop"]`, () => {
+            if (this.hasStarted == false) {
+                this.nonStopExp = true;
+            }
             this.controller();
         });
     }
@@ -21,6 +29,7 @@ class ballExp {
     controller() {
         if (this.hasStarted) {
             this.hasStarted = false;
+            this.nonStopExp = false;
             this.stopUpgrading();
         } else {
             this.hasStarted = true;
@@ -73,22 +82,25 @@ class ballExp {
         var expKuli = parseInt(expKuliPodzielony[0]);
         var expKuliPotrzebny = parseInt(expKuliPodzielony[1]);
         
-        if (expKuli < expKuliPotrzebny) {
+        if (expKuli < expKuliPotrzebny || this.nonStopExp) {
             GAME.emitOrder({ a: 45, type: 3, bid: GAME.ball_id });
         } else {
             this.stopUpgrading();
         }
     }
 
-    showExpButton() {
+    showExpButtons() {
         $("#soulstone_interface > div.pull-left.ball_stats > div > div.main_bar").after('<button id="ss_lvlup_next" class="btn_small_gold option" data-option="ss_lvlup_next">Exp do NEXT</button>');
+        $("#soulstone_interface > div.pull-left.ball_stats > div > div.main_bar").after('<button id="ss_lvlup_nonstop" class="btn_small_gold option" data-option="ss_lvlup_nonstop">Exp bez przerwy</button>');
     }
 
     switchButtonText() {
         $("#ss_lvlup_next").html(this.hasStarted ? this.stopText : this.expText);
+        $("#ss_lvlup_nonstop").html(this.hasStarted ? this.stopText : this.expNonStopText);
     }
 
-    hideExpButton() {
+    hideExpButtons() {
         $("#ss_lvlup_next").remove();
+        $("#ss_lvlup_nonstop").remove();
     }
 }
