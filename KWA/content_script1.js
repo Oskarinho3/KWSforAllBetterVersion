6ab117352fef093d81bc72d183384a4b5fe5e3d7
KWS4ALL <kwsforall@proton.me>
2024-02-28 08:28:17 +0100

    fix source links

--- KWA/content_script1.js
+++ KWA/content_script1.js
@@ -4,11 +4,11 @@
      scr.innerHTML = `const LoadScript = (url, cb) => { fetch(url).then(res => res.json()).then((out) => { cb(out) }).catch(err => { throw err }); }
  
  const links = {
-    "link1":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballExp.js",
-    "link2":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballUpgrade.js",
-    "link3":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballReset.js",
-    "link4":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballManager.js",
-    "link5":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/script1-2.js",
+    "link1":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/main/ballExp.js",
+    "link2":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/main/ballUpgrade.js",
+    "link3":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/main/ballReset.js",
+    "link4":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/main/ballManager.js",
+    "link5":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/main/script1-2.js",
  };
  
  const fetchPromises = Object.values(links).map(link => {
