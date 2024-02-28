bdebb2655355904415f60ba7ab05307ce9afc306
KWS4ALL <kwsforall@proton.me>
2024-02-28 08:27:27 +0100

    Get sources from development branch for a dev version

--- KWA/content_script1.js
+++ KWA/content_script1.js
@@ -4,11 +4,11 @@
      scr.innerHTML = `const LoadScript = (url, cb) => { fetch(url).then(res => res.json()).then((out) => { cb(out) }).catch(err => { throw err }); }
  
  const links = {
-    "link1":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballExp.js",
-    "link2":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballUpgrade.js",
-    "link3":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballReset.js",
-    "link4":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/ballManager.js",
-    "link5":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/feature/main/script1-2.js",
+    "link1":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/development/ballExp.js",
+    "link2":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/development/ballUpgrade.js",
+    "link3":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/development/ballReset.js",
+    "link4":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/development/ballManager.js",
+    "link5":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/development/script1-2.js",
  };
  
  const fetchPromises = Object.values(links).map(link => {
